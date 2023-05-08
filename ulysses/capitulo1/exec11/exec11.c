5fe0e867e8f2d8032b40b3cbd031d3954c8d62bb
Reyralona <91705984+Reyralona@users.noreply.github.com>
2023-05-08 13:49:38 -0300

    Delete ulysses/capitulo1 directory

--- ulysses/capitulo1/exec11/exec11.c
+++
@@ -1,7 +0,0 @@
-/*
-
-11 . Quais são os operadores da linguagem C que possuem ordem de avaliação de operandos definida?
-     
-    Os operadores de conjunção e disjunção lógicas (&& = AND, || = OR). Esses operadores possuem ordem de avaliação.
-
-*/
